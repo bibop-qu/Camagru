a9eaf421709a617884f767bd47445b1a3d6a572e
bibop-qu <lequereb@gmail.com>
2016-10-13 15:22:19 +0200

    first push

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>Camagru</title>
+		<meta charset="UTF-8">
+		<meta name="author" content="basle-qu">
+		<meta name="description" content="instagram-like">
+		<link rel="stylesheet" type="text/css" href="style/design.css">
+	</head>
+	<body>
+		<header></header>
+		<section></section>
+		<aside></aside>
+		<footer></footer>
+	</body>
+</html>
